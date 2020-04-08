8cac2fd08483c11b1f630189eed903db00bc3c80
Joel Rodriguez <joelrodriguez@Joels-MacBook-Pro.local>
2020-04-08 07:02:21 -0400

    Updates

---
+++ precache-manifest.a5280b33e700b9ee55a077fe6d7cd5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a09ccfba825849d23394fdf23674415",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d033d6be0219fc08816b",
+    "url": "/monsters-rolodex/static/css/main.0f4d4f12.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d033d6be0219fc08816b",
+    "url": "/monsters-rolodex/static/js/main.68095ce5.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
